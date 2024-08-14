caf3bef57f2da90e93a898a475c5fa72732a16a4
Mike_Sognatore <mikepascal.delta@gmail.com>
2024-08-14 11:46:25 -0400

    .....

--- app/src/app/ui/dashboard/user/status/statusView.tsx
+++ app/src/app/ui/dashboard/user/status/statusView.tsx
@@ -43,7 +43,7 @@ export function StatusView({userStatus}: {userStatus: UserStatus[]})
                 <div className="bg-gray-800 p-2">
                     <StatusTopNav statusId={statusId} setStatusId={setStatusId} status={{current: userStatus[statusId], all: userStatus}} />
                 </div>
-                <div>
+                <div className="">
                     {
                         userStatus.map((status_, i) =>{
                             if(status_.type == 'video')
@@ -55,7 +55,7 @@ export function StatusView({userStatus}: {userStatus: UserStatus[]})
                                         onTouchMove={handleTouchMove}
                                         onTouchEnd={handleTouchEnd}
                                     >
-                                        {statusId === i && <video onEnded={()=>next()} autoPlay key={i=1} className={"h-[70vh] w-full" + (statusId === i ? "" : " hidden")} controls preload="none">
+                                        {statusId === i && <video onEnded={()=>next()} autoPlay key={i=1} className={"max-h-[70vh] w-full" + (statusId === i ? "" : " hidden")} controls preload="none">
                                             <source key={i+2} src={status_.src} type="video/mp4" />
                                             Your browser does not support the video tag.
                                         </video>}
@@ -67,7 +67,7 @@ export function StatusView({userStatus}: {userStatus: UserStatus[]})
                                         onTouchStart={handleTouchStart}
                                         onTouchMove={handleTouchMove}
                                         onTouchEnd={handleTouchEnd}
-                                        onClick={()=>next()} key={i+4} className={"bg-gray-600 w-full h-[70vh] aspect-square animate-fadeOut" + (statusId === i ? "" : " hidden")} alt="demo-status-image"
+                                        onClick={()=>next()} key={i+4} className={"bg-gray-600 w-full max-h-[70vh] aspect-square animate-fadeOut" + (statusId === i ? "" : " hidden")} alt="demo-status-image"
                                         src={status_.src}
                                     />
                                 )

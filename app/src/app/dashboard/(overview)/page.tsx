c61885e1f0e2de8b7a308a9e348f67b9bf751257
Mike_Sognatore <mikepascal.delta@gmail.com>
2024-08-14 12:33:36 -0400

    ....demo-post-img

--- app/src/app/dashboard/(overview)/page.tsx
+++ app/src/app/dashboard/(overview)/page.tsx
@@ -6,9 +6,9 @@ export default function Page()
         <div className="w-full max-w-[100vw]">
             <StatusWrapper />
             <div>
-                <img className="w-[600px]" src="http://localhost:3000/_next/image?url=%2Flogo%2Ftalkspace-banner.png&w=256&q=75" alt="img_teste" />
-                <img className="w-[600px]" src="http://localhost:3000/_next/image?url=%2Flogo%2Ftalkspace-banner.png&w=256&q=75" alt="img_teste" />
-                <img className="w-[600px]" src="http://localhost:3000/_next/image?url=%2Flogo%2Ftalkspace-banner.png&w=256&q=75" alt="img_teste" />
+                <img className="w-[600px]" src="https://qaziclinic.com/wp-content/uploads/2021/01/img3-5.jpg" alt="img_teste" />
+                <img className="w-[600px]" src="https://qaziclinic.com/wp-content/uploads/2021/01/img3-5.jpg" alt="img_teste" />
+                <img className="w-[600px]" src="https://qaziclinic.com/wp-content/uploads/2021/01/img3-5.jpg" alt="img_teste" />
             </div>
         </div>
     )

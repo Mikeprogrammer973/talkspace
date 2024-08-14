7eab660cc53d9e16fdd84ddab01dfa8f73951d1b
Mike_Sognatore <mikepascal.delta@gmail.com>
2024-08-14 10:44:02 -0400

    .....

--- app/src/app/login/page.tsx
+++ app/src/app/login/page.tsx
@@ -6,15 +6,21 @@ import LoginForm from "../ui/auth/login-form";
 import { Alert } from "../ui/global/alert";
 import MsgBox from "../ui/global/msgBox";
 
-export default function LoginPage() {
+function LoginComponent()
+{
   const [msgBV, setMsgBV] = useState<boolean>(true)
+  const msg: string | null = new URLSearchParams(useSearchParams()).get("message")
 
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        {new URLSearchParams(useSearchParams()).get("message") === "reg" && <MsgBox setVisible={setMsgBV} visible={msgBV} msg={<Alert title="TalkSpace" color="success" msg={<div className="font-semibold text-lg"> Account created successfully, you can login now. </div>} />} />}
-      </Suspense>
+      {msg === "reg" && <MsgBox setVisible={setMsgBV} visible={msgBV} msg={<Alert title="TalkSpace" color="success" msg={<div className="font-semibold text-lg"> Account created successfully, you can login now. </div>} />} />}
       <LoginForm />
     </div>
   );
+}
+
+export default function LoginPage() {
+  return <Suspense fallback={<div>Loading...</div>}>
+    <LoginComponent />
+  </Suspense>
 }

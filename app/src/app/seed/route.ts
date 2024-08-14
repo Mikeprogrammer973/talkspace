0dc59d2fdf48ee4f747c68d18f4fb25d852c525a
Mike_Sognatore <mikepascal.delta@gmail.com>
2024-08-14 10:16:21 -0400

    .....

--- app/src/app/seed/route.ts
+++ app/src/app/seed/route.ts
@@ -5,19 +5,19 @@ const prisma = new PrismaClient()
 
 export async function GET()
 {
-    const nweUser = await prisma.user.create({
-        data: {
-            email: "thalita@hotmail.com",
-            name: "Thalita Furtado",
-            username: "thalita_f",
-            password: "186464616",
-            profile: {
-                create: {
-                    bio: "New user"
-                }
-            }
-        }
-    })
+    // const nweUser = await prisma.user.create({
+    //     data: {
+    //         email: "thalita@hotmail.com",
+    //         name: "Thalita Furtado",
+    //         username: "thalita_f",
+    //         password: "186464616",
+    //         profile: {
+    //             create: {
+    //                 bio: "New user"
+    //             }
+    //         }
+    //     }
+    // })
 
-    return NextResponse.json(nweUser)
+    // return NextResponse.json(nweUser)
 }

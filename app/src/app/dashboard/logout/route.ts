97088641fc069a858cb16a6165c24729f2697b8e
Mike_Sognatore <mikepascal.delta@gmail.com>
2024-08-14 10:34:50 -0400

    ...

--- app/src/app/dashboard/logout/route.ts
+++ app/src/app/dashboard/logout/route.ts
@@ -1,7 +1,8 @@
+import { NextResponse } from "next/server";
 import { signOut } from "tspace/app/lib/user/auth/auth";
 
 
 export async function GET() {
     // await signOut()
-    return Promise.resolve()
+    return NextResponse.json({})
 }

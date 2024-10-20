883912e51598f68f134f0ca56b6d973dbfa522ff
Mike D. Pascal <96785933+Mikeprogrammer973@users.noreply.github.com>
2024-10-19 21:06:53 -0400

    Update header.tsx

--- app/src/app/ui/global/header.tsx
+++ app/src/app/ui/global/header.tsx
@@ -64,10 +64,10 @@ export default function InitHeader({selectedPage}: {selectedPage: number}) {
               <div className="hidden md:block">
                 <div className="ml-4 flex items-center md:ml-6">
 
-                  {/* Profile dropdown */}
+                  {/* Report dropdown */}
                   <Menu as="div" className="relative ml-3">
                     <div>
-                      <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-950 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-950">
                         <span className="absolute -inset-1.5" />
                         <span className="sr-only">Open report menu</span>
                         <img alt="" src={report.imageUrl} className="h-8 w-8 rounded-full bg-white" />
@@ -93,7 +93,7 @@ export default function InitHeader({selectedPage}: {selectedPage: number}) {
               </div>
               <div className="-mr-2 flex md:hidden">
                 {/* Mobile menu button */}
-                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-950 p-2 text-gray-400 hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-950">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
                   <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />

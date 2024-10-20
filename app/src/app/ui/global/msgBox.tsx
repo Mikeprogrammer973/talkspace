296b0bd688f56fc21a7ecd847957ebeb64adc5f2
Mike_Sognatore <mikepascal.delta@gmail.com>
2024-10-20 15:00:52 -0400

    .....

--- app/src/app/ui/global/msgBox.tsx
+++ app/src/app/ui/global/msgBox.tsx
@@ -14,7 +14,7 @@ export default function MsgBox(prms: MsgPrms)
     return(
         prms.visible ? <section onClick={(e)=>{
             prms.setVisible(false)
-        }} style={{backgroundColor: "rgb(0, 0, 0, 0.8)", zIndex: 2}} className="w-full h-full fixed top-0 left-0 flex items-center justify-center">
+        }} style={{backgroundColor: "rgb(0, 0, 0, 0.8)", zIndex: 2}} className="w-full h-full fixed top-0 left-0 z-50 flex items-center justify-center">
         <div className={"inline-block" + (prms.status ? " w-full h-screen rounded-none shadow-none bg-transparent" : " bg-gray-900 max-w-[90%] max-h-[90%] rounded-xl shadow-lg overflow-y-scroll scrollbar-none")}>
             {prms.msg}
         </div>
